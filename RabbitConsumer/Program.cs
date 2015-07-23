1d324a76d95891ea2dc4f2c8aa19f891bd505b80
Safin Ruslan <ruslan.safin@byndyusoft.com>
2015-07-23 21:53:31 +0500

    initial commit

---
+++ RabbitConsumer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitConsumer
+{
+    class Program
+    {
+        private const string queueName = "myQueue";
+
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost"
+            };
+
+            using (IConnection connection = connectionFactory.CreateConnection())
+            using (IModel chanell = connection.CreateModel())
+            {
+                chanell.QueueDeclare(queueName, false, false, false, null);
+
+                var consumer = new QueueingBasicConsumer(chanell);
+                chanell.BasicQos(0, 1, false);
+                //true
+                chanell.BasicConsume(queueName, false, consumer);
+
+                while (true)
+                {
+                    var message = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+                    var body = Encoding.UTF8.GetString(message.Body);
+
+                    Thread.Sleep(body.Length * 1000);
+                    Console.WriteLine("Received message: " + body);
+                    
+                    chanell.BasicAck(message.DeliveryTag, false);
+                }
+            }
+        }
+    }
+}
---
+++ RabbitPublisher/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitPublisher
+{
+    internal class Program
+    {
+        private const string queueName = "myQueue";
+
+        private static void Main()
+        {
+            var connectionFactory = new ConnectionFactory
+                                        {
+                                            HostName = "localhost"
+                                        };
+
+            while (true)
+            {
+                var message = Console.ReadLine();
+
+                using (IConnection connection = connectionFactory.CreateConnection())
+                using (IModel chanell = connection.CreateModel())
+                {
+                    chanell.QueueDeclare(queueName, false, false, false, null);
+
+                    byte[] payload = Encoding.UTF8.GetBytes(message ?? "empty string");
+                    chanell.BasicPublish("", queueName, chanell.CreateBasicProperties(), payload);
+                    Console.WriteLine("Sent message");
+                }
+            }
+        }
+    }
+}
